ab88824541d7527444c48a00c304104c230c0dff
turbo <sjkang.turbo@gmail.com>
2022-03-30 03:55:22 +0900

    change okex client library

--- server/go.mod
+++ server/go.mod
@@ -2,29 +2,8 @@ module github.com/TurboKang/newton
 
 go 1.18
 
-require github.com/nntaoli-project/goex v1.3.1
+require github.com/amir-the-h/okex v1.0.26-alpha
 
-require (
-	github.com/Kucoin/kucoin-go-sdk v1.2.7 // indirect
-	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/go-openapi/errors v0.19.4 // indirect
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/klauspost/compress v1.15.1 // indirect
-	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
-	github.com/nubo/jwt v0.0.0-20150918093313-da5b79c3bbaf // indirect
-	github.com/pkg/errors v0.8.1 // indirect
-	github.com/sirupsen/logrus v1.4.1 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.34.0 // indirect
-	golang.org/x/net v0.0.0-20220325170049-de3da57026de // indirect
-	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	gorm.io/driver/mysql v1.3.2 // indirect
-	gorm.io/gorm v1.23.3 // indirect
-)
+require github.com/gorilla/websocket v1.5.0 // indirect
 
-replace github.com/nntaoli-project/goex => github.com/TurboKang/goex v1.0.9-0.20220322080242-32edb26eeacf
+replace github.com/amir-the-h/okex => github.com/TurboKang/okex v1.0.27-beta
--- server/main.go
+++ server/main.go
@@ -1,17 +1,49 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
+	"time"
 
-	"github.com/nntaoli-project/goex"
-	"github.com/nntaoli-project/goex/builder"
+	"github.com/amir-the-h/okex"
+	"github.com/amir-the-h/okex/api"
+	"github.com/amir-the-h/okex/requests/rest/market"
 )
 
 func main() {
 	apiKey := os.Getenv("API_KEY")
 	secretKey := os.Getenv("SECRET_KEY")
-	passPhrase := os.Getenv("PASS_PHRASE")
-	api := builder.DefaultAPIBuilder.APIKey(apiKey).APISecretkey(secretKey).ApiPassphrase(passPhrase).Build(goex.OKEX) //创建现货api实例
-	log.Println(api.GetTicker(goex.ETH_USDT))
+	passphrase := os.Getenv("PASS_PHRASE")
+
+	dest := okex.NormalServer // The main API server
+	ctx := context.Background()
+	client, err := api.NewClient(ctx, apiKey, secretKey, passphrase, dest)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	response, err := client.Rest.Market.GetCandlesticksHistory(market.GetCandlesticks{
+		InstID: "BTC-USDT-SWAP",
+		Bar:    okex.Bar15m,
+		After:  time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC).UnixMilli(),
+		Limit:  10,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, candle := range response.Candles {
+		log.Printf("Candles %+v", candle)
+	}
+	t := (*time.Time)(&response.Candles[len(response.Candles)-1].TS)
+	response, err = client.Rest.Market.GetCandlesticksHistory(market.GetCandlesticks{
+		InstID: "BTC-USDT-SWAP",
+		Bar:    okex.Bar15m,
+		After:  t.UnixMilli(),
+		Limit:  10,
+	})
+
+	for _, candle := range response.Candles {
+		log.Printf("Candles2 %+v", candle)
+	}
 }
